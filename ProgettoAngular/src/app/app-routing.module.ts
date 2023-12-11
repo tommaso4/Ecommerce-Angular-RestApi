0f57a232126c3b5c83f4ecffcf9da9b0c3672c4e
LorenzoN13 <lorenzoneroni13@gmail.com>
2023-12-11 15:16:16 +0100

    .

--- ProgettoAngular/src/app/app-routing.module.ts
+++ ProgettoAngular/src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-<<<<<<< HEAD
-const routes: Routes = [{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }];
-=======
 const routes: Routes = [{ path: 'LogSystem',
  loadChildren: () => import('./pages/log-system/log-system.module').then(m => m.LogSystemModule) }];
->>>>>>> 3e2a592459deb573f0acb3d019a65907ced2f7ef
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
