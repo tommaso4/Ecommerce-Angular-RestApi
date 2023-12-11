ed4aea2ce9a307e4ba1825572cc6daee471a028e
LorenzoN13 <lorenzoneroni13@gmail.com>
2023-12-11 22:26:10 +0100

    in corso per le birre rosse

--- ProgettoAngular/src/app/services/beer.service.ts
+++ ProgettoAngular/src/app/services/beer.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Ibeer } from '../Modules/ibeer';
+import { Observable, filter, map } from 'rxjs';
+
+type responseData = {
+  beer:Ibeer[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class BeerService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  apiUrl:string = 'http://localhost:3000/beer'
+
+  getRedBeer(): Observable<Ibeer[]> {
+    return this.http.get<Ibeer[]>(this.apiUrl)
+    .pipe(map((beer:Ibeer[]) => beer.filter(birra => birra.volume.tipologia === 'rossa')));
+  }
 }
