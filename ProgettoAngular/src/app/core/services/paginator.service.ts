d362250f365afa3baf57ceb5e7bf32791b469e07
calogeroteresi <caloteresi@gmail.com>
2023-12-12 12:40:46 +0100

    merge

--- ProgettoAngular/src/app/core/services/paginator.service.ts
+++ ProgettoAngular/src/app/core/services/paginator.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Beer } from '../models/beer.model';
+
 
 
 @Injectable({
--- ProgettoAngular/src/app/pages/home/beer-card/beer-card.component.ts
+++ ProgettoAngular/src/app/pages/home/beer-card/beer-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Beer } from '../../../core/models/beer.model';
+
 import { Ibeer } from '../../../Modules/ibeer';
 
 
