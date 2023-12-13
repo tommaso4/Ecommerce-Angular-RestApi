3f79df30197c43dbe3c0360d2c022084cd4eae08
Emilio-Plances <emilio.plances@gmail.com>
2023-12-13 11:33:21 +0100

    Edit Account ready

--- ProgettoAngular/src/app/pages/user-profile/edit-profile/edit-profile.component.ts
+++ ProgettoAngular/src/app/pages/user-profile/edit-profile/edit-profile.component.ts
@@ -1,9 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup, Validators, FormControl, ValidationErrors, FormBuilder } from '@angular/forms';
-import { tap, catchError } from 'rxjs';
+import { catchError } from 'rxjs';
 import { LogSystemService } from '../../../services/log-system.service';
-import { IUser } from '../../../Modules/iuser';
-import { Router } from '@angular/router';
+import { IUserAuth } from '../../../Modules/iuser-auth';
 
 @Component({
   selector: 'app-edit-profile',
@@ -14,26 +13,29 @@ export class EditProfileComponent {
 
   @Input() editMode!:boolean;
 
+  regExPassword:string=`^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=[^0-9]*[0-9]).{8,}$`
   form!: FormGroup;
   confirmPassword!:string;
   loading!:boolean;
-  user:IUser|undefined;
+  userAuth:IUserAuth|undefined;
 
   constructor(
     private fb:FormBuilder,
     private LSS:LogSystemService,
-    private router:Router
     ){
-    this.LSS.user$.subscribe(user =>this.user=user?.user);
+    this.LSS.user$.subscribe(user =>{
+      if(!user)  return
+      this.userAuth=user
+    });
   }
 
   ngOnInit(){
-    if(!this.user) return;
+    if(!this.userAuth) return;
     this.form = this.fb.group({
-      name: this.fb.control(this.user?.name,[Validators.required]),
-      surname: this.fb.control(this.user?.surname,[Validators.required]),
-      email: this.fb.control(this.user?.email,[Validators.email]),
-      password: this.fb.control(null),
+      name: this.fb.control(this.userAuth?.user.name,[Validators.required]),
+      surname: this.fb.control(this.userAuth?.user.surname,[Validators.required]),
+      email: this.fb.control(this.userAuth?.user.email,[Validators.email]),
+      password: this.fb.control(null,[Validators.required,Validators.pattern(this.regExPassword)]),
 
     })
   }
@@ -66,20 +68,19 @@ export class EditProfileComponent {
 
   submit(){
     this.loading=true;
-    this.form.value.password=this.user?.password;
-    console.log(this.user);
-
-    if(!this.user) return
-    console.log(this.form.value);
-
-    this.LSS.updateUser(this.user?.id,this.form.value).pipe(tap(()=>{
-      this.loading=false;
-      this.editMode=false;
-      this.router.navigate(['/userprofile']);
-      this.user=this.form.value
-    }),catchError(err=>{
+    if(!this.userAuth) return
+    this.form.value.id=this.userAuth.user.id
+    this.userAuth.user=this.form.value
+    this.LSS.updateUser(this.userAuth).pipe(catchError(err=>{
       this.loading=false
-      throw err
-    })).subscribe()
+      throw err;
+    }))
+    .subscribe(data=>{
+      if(!this.userAuth) return;
+      this.loading=false;
+      this.userAuth.user=data;
+      localStorage.setItem('user', JSON.stringify(this.userAuth))
+      window.location.reload();
+    })
   }
 }
