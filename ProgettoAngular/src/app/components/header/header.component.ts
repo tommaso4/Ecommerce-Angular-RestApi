796f2bfd98fb3d27c617a0309312ecc234bc0ae2
Tommaso Cantarini <tommasocantarini4@gmail.com>
2023-12-15 16:04:52 +0100

    fine

--- ProgettoAngular/src/app/components/header/header.component.ts
+++ ProgettoAngular/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { LogSystemService } from '../../services/log-system.service';
 import { IUserAuth } from '../../Modules/iuser-auth';
+import { RolesService } from '../../services/roles.service';
 
 
 @Component({
@@ -13,12 +14,15 @@ export class HeaderComponent implements OnInit {
   isLogged = false;
   user!: IUserAuth;
   toggle: boolean=true;
+  admin: boolean = false;
 
   constructor(
     private LSS: LogSystemService,
+    private roleSvc: RolesService
   ) {}
 
   ngOnInit() {
+    this.roleSvc.userRole$.subscribe(user=> this.admin = user?.role== 'admin' ? true : false)
     this.LSS.user$.subscribe((user: IUserAuth | null) => {
       this.isLogged = !!user;
       this.user = user!;
