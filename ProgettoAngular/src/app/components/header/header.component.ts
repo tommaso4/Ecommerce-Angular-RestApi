68e19723b112c26727efd833e526bd8c5b3c594d
Emilio-Plances <emilio.plances@gmail.com>
2023-12-15 15:23:31 +0100

    header

--- ProgettoAngular/src/app/components/header/header.component.ts
+++ ProgettoAngular/src/app/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { LogSystemService } from '../../services/log-system.service';
 import { IUserAuth } from '../../Modules/iuser-auth';
-import { CartService } from '../../services/cart.service';
+
 
 @Component({
   selector: 'app-header',
@@ -12,6 +12,7 @@ export class HeaderComponent implements OnInit {
 
   isLogged = false;
   user!: IUserAuth;
+  toggle: boolean=true;
 
   constructor(
     private LSS: LogSystemService,
