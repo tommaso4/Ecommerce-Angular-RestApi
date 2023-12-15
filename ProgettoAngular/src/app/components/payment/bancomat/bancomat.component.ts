1f86603f2190c775f7b3f7235b25b51b3aa176fa
calogeroteresi <caloteresi@gmail.com>
2023-12-15 16:01:34 +0100

    paypal

--- ProgettoAngular/src/app/components/payment/bancomat/bancomat.component.ts
+++ ProgettoAngular/src/app/components/payment/bancomat/bancomat.component.ts
@@ -5,6 +5,7 @@ import { LogSystemService } from '../../../services/log-system.service';
 import { BeerService } from '../../../services/beer.service';
 import { Router } from '@angular/router';
 import { CartService } from '../../../services/cart.service';
+import { IShop } from '../../../Modules/ishop';
 
 @Component({
   selector: 'app-bancomat',
@@ -21,12 +22,16 @@ export class BancomatComponent {
     private logSvc : LogSystemService,
     private cartSvc : CartService,
     private router: Router){}
-
+    allItem : IShop[] | null =[];
 
     ngOnInit() {
       this.cartSvc.cart$.subscribe((total: number) => {
         this.totalCart = total;
       });
+      this.cartSvc.allItem$.subscribe((items) => {
+        this.allItem = items;
+        console.log(this.allItem);
+    })
     }
 
   submitForm(form: NgForm) {
@@ -43,10 +48,29 @@ export class BancomatComponent {
         }).then(() => {
           this.router.navigate(['/home']);
         });
-
+        setTimeout(() => {
+          this.deleteAll();
+        }, 2000);
       })
     } else {
       console.log('Il modulo Bancomat non è valido.');
     }
+  }
+
+  deleteAll() {
+    if(!this.allItem)return;
+    this.allItem.forEach(item => {
+     this.cartSvc.deleteCart(item.id).subscribe({
+      next: (data: any) => {
+        this.allItem=data;
+        this.cartSvc.setTotalCart(0);
+        this.totalCart = 0;
+        this.cartSvc.setAllItemSubject([])
+      },
+      error: (error) => {
+        console.error(`Errore durante la cancellazione degli elementi. URL: `, error);
+      }
+    });
+  });
   }
 }
